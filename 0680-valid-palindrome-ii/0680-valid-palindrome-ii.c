df58767bb49030dfe787715f6f7b179f6f2de48b
kartikeyan sahani <kk20140158570@gmail.com>
2026-03-04 00:02:14 +0530

    Time: 0 ms (100%), Space: 11.9 MB (5.22%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.c
@@ -0,0 +1,26 @@
+bool palindromehelper(char*s, int i, int j){
+    while(i<j){
+        if(s[i] != s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;   
+    }
+
+
+bool validPalindrome(char* s) {
+   int i = 0;
+   int j = strlen(s) - 1;
+   while ( i < j){
+        if (s[i] != s[j]){
+            return palindromehelper(s, i+1, j) || palindromehelper(s, i, j-1);   
+            }
+        
+        i++;
+        j--;       
+   }
+        return true;
+    }
+
