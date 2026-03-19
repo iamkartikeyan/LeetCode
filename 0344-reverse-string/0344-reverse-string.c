8a2841f027bf3e6e547e46c9319891f148655284
kartikeyan sahani <kk20140158570@gmail.com>
2026-03-19 23:47:34 +0530

    Time: 1 ms (8%), Space: 17.9 MB (10.47%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.c
+++ 0344-reverse-string/0344-reverse-string.c
@@ -1,12 +1,12 @@
 void reverseString(char* s, int sSize) {
-    int i = 0;
-    int j = sSize - 1;
-    while (i < j ){
-        int temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-        i++;
-        j--;
-
+    int start = 0;
+    int last = sSize - 1;
+    while(start<last){
+        int temp;
+        temp = s[start];
+        s[start] = s[last];
+        s[last] = temp;
+        start++;
+        last--;        
     }
 }
