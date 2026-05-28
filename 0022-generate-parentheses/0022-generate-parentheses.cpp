e9c8c69803ea05268824244e2e7e3731b8a8585e
kartikeyan sahani <kk20140158570@gmail.com>
2026-05-29 00:17:27 +0530

    Time: 2 ms (81.46%), Space: 13.1 MB (78.6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,63 +1,29 @@
-// class Solution {
-// public:
-
-//     void paranth(int left, int right, int n , vector<string> &ans ,string temp ){
-//         if ((left == n) && (right == n)){
-//             ans.push_back(temp);
-//             return;
-//         }
-
-//         if ((right < left) && right < n){
-            
-//             temp.push_back(')');
-//             paranth(left, right+1, n, ans, temp);
-//         }
-        
-//         if (left < n){
-//             temp.pop_back();
-//             temp.push_back('(');
-
-//             paranth(left + 1, right, n, ans, temp);
-//         }
-        
-//     }
-//     vector<string> generateParenthesis(int n) {
-//         int left ;
-//         int right;
-//         string temp;
-//         vector<string> ans;
-//         paranth(0, 0, n, ans, temp);
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
 
-    void paranth(int left, int right, int n,
-             vector<string> &ans, string temp) {
+    void paranth(int left, int right, int n, vector<string> &ans, string &temp) {
 
         if (left == n && right == n) {
             ans.push_back(temp);
             return;
         }
 
-        if (right < left && right < n) {
-            string temp1 = temp;
-            temp1.push_back(')');
-            paranth(left, right + 1, n, ans, temp1);
+        if (left < n) {
+            temp.push_back('(');
+            paranth(left + 1, right, n, ans, temp);
+            temp.pop_back();
         }
 
-        if (left < n) {
-            string temp2 = temp;
-            temp2.push_back('(');
-            paranth(left + 1, right, n, ans, temp2);
+        if (right < left) {
+            temp.push_back(')');
+            paranth(left, right + 1, n, ans, temp);
+            temp.pop_back(); 
         }
     }
 
     vector<string> generateParenthesis(int n) {
-        string temp;
         vector<string> ans;
+        string temp;
         paranth(0, 0, n, ans, temp);
         return ans;
     }
