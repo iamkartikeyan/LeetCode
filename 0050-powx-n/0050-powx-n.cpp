1557cf9cf32c7b98813c314ada3d5b4654fa6c12
kartikeyan sahani <kk20140158570@gmail.com>
2026-05-31 16:07:04 +0530

    Time: 3 ms (2.32%), Space: 8.7 MB (42.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
+    void pow(double x, long long &N, double &value ){
+        if ( N == 0 ){
+            return;
+        }
+        
+        if (N % 2 == 0){
+            x = x * x;
+            N = N/2;
+            pow(x, N, value);
+            
+        }
+        else{
+            N = N -1;
+            value = value * x;
+            pow(x, N, value);
+            cout << N;
+        }
+    }
     double myPow(double x, int n) {
-        double ans = pow(x, n);
-        return ans;
+        double value = 1; 
+        long long N = n;
+        if( n < 0){
+            x = 1.0000/x;
+            N = -N;
+        }
+        pow( x, N , value);
+        return value;
     }
 };
