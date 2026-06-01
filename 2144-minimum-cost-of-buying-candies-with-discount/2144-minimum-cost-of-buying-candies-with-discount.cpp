8f42d691748bdee87b9860cb37e9c0cfcb782a6d
kartikeyan sahani <kk20140158570@gmail.com>
2026-06-02 01:16:40 +0530

    Time: 0 ms (100%), Space: 14.3 MB (49.81%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+
+        sort(cost.begin(), cost.end(), greater<int>());
+        int i = 0;
+        int count = 0;
+        for( i = 0; i < (cost.size()/3)*3 ; i = i+3){
+            int k = cost[i];
+            for (int j = i ; j < i + 3; j++){
+                count = count + cost[j];
+                if (cost[j]< k){
+                    k = cost[j];
+                }
+            } 
+                count = count - k;   
+
+            
+        }
+        cout << count;
+        while ( i < cost.size()){
+            count = count + cost[i];
+            i++;
+        }
+        return count ;
+    }
+};
