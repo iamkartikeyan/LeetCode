46e4cf01c1105715b7a5d20d6dcc5ce29bb994cf
kartikeyan sahani <kk20140158570@gmail.com>
2026-07-18 14:25:23 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    void swap(int &a, int &b){
-        int temp = a;
-        a = b;
-        b = temp;
-    }
-
     void sortColors(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
         int i = 0;
-        int numssize = nums.size();
-        int j = i ; // j should be equal to i from middle pointer 
-        int k = numssize - 1;
-        while(j <= k){
-            if (nums[j] == 0){
-                swap(nums[i], nums[j]);
+        while ( i <= right)
+            if (nums[i] == 2){
+                swap(nums[i], nums[right]);
+                right--;
+            }
+            else if (nums[i] == 1){
                 i++;
-                j++; //we need to increase this j++ its compulsory 
             }
-            else if (nums[j]== 1){
-                j++;
-            }
-            else{
-                swap(nums[j], nums[k]);
-                k--;
+
+            else {
+                swap(nums[left], nums[i]);
+                left++;
+                i++;
             }
         }
-    }
+    
 };
-
